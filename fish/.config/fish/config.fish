f34706d65b8f50d12f454e0a849b3c98ff0f4078
Jason Mason <jasonjaymason@gmail.com>
2023-10-24 20:49:18 -0600

    Edited some settings in xfce themes

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    function is_valid_command
+        if which $argv[1] > /dev/null
+            true
+        else
+            false
+        end
+    end
+
+    # logo-ls is for icons for ls https://github.com/Yash-Handa/logo-ls
+    if is_valid_command logo-ls
+      alias ls="logo-ls"
+    end
+
+    # Air is a hot reload for go
+    alias air="~/go/bin/air"
+
+    alias n="nvim"
+
+    #useful functions
+    #cd and ls
+    function cdl
+      cd $argv && ls
+    end
+
+    #z and ls
+    function zl
+      z $argv && ls
+    end
+
+    fish_add_path -p /etc/profile ~/.bash_profile /etc/bash.bashrc ~/.bashrc /usr/local/go/bin
+    set fish_greeting
+
+    # Icons for hydro theme https://github.com/jorgebucaran/hydro
+    set --global hydro_symbol_prompt '󰈺'
+    set --global hydro_symbol_git_dirty ' ≠ '
+    set --global hydro_symbol_git_ahead '↑  '
+    set --global hydro_symbol_git_behind '↓  '
+    set --global hydro_color_pwd $fish_color_cwd
+    set --global hydro_color_prompt $fish_color_param
+end
