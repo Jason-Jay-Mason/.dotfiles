973e016a0985d4c97375541b1777bfe90ee27960
Jason Mason <jasonjaymason@gmail.com>
2024-01-10 13:10:44 -0700

    Made telescope and nvim tree ignore go files produced by templ

--- neovim/.config/nvim/lua/plugins/config/telescope.lua
+++ neovim/.config/nvim/lua/plugins/config/telescope.lua
@@ -1,49 +1,49 @@
-local ok, telescope = pcall(require, 'telescope')
+local ok, telescope = pcall(require, "telescope")
 
 if not ok then
-  return
+	return
 end
 
-local get_window = require('utils').get_popup_size
+local get_window = require("utils").get_popup_size
 
 local actions = require("telescope.actions")
 -- Loads extension for media files
 telescope.load_extension("media_files")
 
 telescope.setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules", "tmp" },
-    results_title = false,
-    dynamic_preview_title = true,
-    prompt_prefix = "   ",
-    layout_strategy = "horizontal",
-    borderchars = {
-      prompt = { "", "", "", "", "", "", "", "" },
-      results = { "", "", "", "", "", "", "", "" },
-      preview = { "", "", "", "", "", "", "", "" },
-    },
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-        results_width = 0.8,
-        preview_cutoff = 0,
-      },
-      width = function()
-        local win = get_window()
-        return win.window_w
-      end,
-      height = function()
-        local win = get_window()
-        return win.window_h
-      end,
-    },
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-c>"] = actions.close,
-      }
-    }
-  }
+	defaults = {
+		file_ignore_patterns = { "node_modules", "tmp", ".*_templ.go" },
+		results_title = false,
+		dynamic_preview_title = true,
+		prompt_prefix = "   ",
+		layout_strategy = "horizontal",
+		borderchars = {
+			prompt = { "", "", "", "", "", "", "", "" },
+			results = { "", "", "", "", "", "", "", "" },
+			preview = { "", "", "", "", "", "", "", "" },
+		},
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+				preview_cutoff = 0,
+			},
+			width = function()
+				local win = get_window()
+				return win.window_w
+			end,
+			height = function()
+				local win = get_window()
+				return win.window_h
+			end,
+		},
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
+			},
+		},
+	},
 })
