4d5c8d0cc1256a7f03c022cf3b19bca06601d4a1
Jason Mason <jasonjaymason@gmail.com>
2025-03-28 13:48:17 -0600

    Change display type

--- neovim/.config/nvim/lua/plugins/config/neogit.lua
+++ neovim/.config/nvim/lua/plugins/config/neogit.lua
@@ -125,14 +125,14 @@ neogit.setup {
     spell_check = true,
   },
   commit_select_view = {
-    kind = "tab",
+    kind = "replace",
   },
   commit_view = {
     kind = "vsplit",
     verify_commit = vim.fn.executable("gpg") == 1, -- Can be set to true or false, otherwise we try to find the binary
   },
   log_view = {
-    kind = "tab",
+    kind = "replace",
   },
   rebase_editor = {
     kind = "auto",
@@ -156,10 +156,10 @@ neogit.setup {
     kind = "split",
   },
   stash = {
-    kind = "tab",
+    kind = "replace",
   },
   refs_view = {
-    kind = "tab",
+    kind = "replace",
   },
   signs = {
     -- { CLOSED, OPENED }
@@ -171,7 +171,7 @@ neogit.setup {
   integrations = {
     -- If enabled, use telescope for menu selection rather than vim.ui.select.
     -- Allows multi-select and some things that vim.ui.select doesn't.
-    telescope = nil,
+    telescope = true,
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
     -- The diffview integration enables the diff popup.
     --
